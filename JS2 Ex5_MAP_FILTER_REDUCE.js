a6e68d9d01a26feb2d2989406eb8227439c3b609
LiliKaori <lilikaori10@gmail.com>
2023-02-24 17:51:52 -0300

    Exerc resolvido

--- JS2 Ex5_MAP_FILTER_REDUCE.js
+++ JS2 Ex5_MAP_FILTER_REDUCE.js
@@ -8,3 +8,29 @@ const companies = [
     { name: "Spotify", marketValue: 30, CEO:"Daniel Ek", foundedOn: 2006},
     { name: "Apple", marketValue: 845, CEO:"Tim Cook", foundedOn: 1976}
 ]
+
+const add10MarketValue = company => {
+    company.marketValue = company.marketValue + company.marketValue * 10/100
+    return company
+}
+
+const OldCompanies = company => company.foundedOn<2000
+
+const AllMarketValue = (acc, current) => acc + current.marketValue
+
+const BestCompanies = companies.map(add10MarketValue).filter(OldCompanies).reduce(AllMarketValue,0)
+console.log(BestCompanies)
+
+
+//Fazendo de outro jeito
+// const Add10Companies2 = companies.map((company2) => {
+//     company2.marketValue = company2.marketValue + company2.marketValue * 10/100
+//     return company2
+// })
+// //console.log(Add10Companies2)
+
+// const OldCompanies2 = Add10Companies2.filter((company2) => company2.foundedOn<2000)
+// //console.log(OldCompanies2)
+
+// const BestCompanies2 = OldCompanies2.reduce((acc, current2) => acc+current2.marketValue, 0)
+// console.log(BestCompanies2)
