67c54f132521cb4e441b3cc3cbbb0d8caa2b547b
LiliKaori <lilikaori10@gmail.com>
2023-02-24 14:51:23 -0300

    Primeiro commit

---
+++ JS2 Ex3_FILTER.js
@@ -0,0 +1,14 @@
+//Filtrar o array list com número pares e múltiplo de 6
+const list = [20,3,232,12,17,541,6,87,275,1000]
+
+
+
+//Filtrar o array companies fundadas depois 1975 e tenha mais de 2 milhões de valor de mercado
+const companies = [
+    { name: "Samsung", marketValue: 50, CEO:"Kim Hyun Suk", fundedOn: 1938},
+    { name: "Microsoft", marketValue: 415, CEO:"Satya Nadella", fundedOn: 1975},
+    { name: "Intel", marketValue: 117, CEO:"Brian Krzanich", fundedOn: 1968},
+    { name: "Facebook", marketValue: 383, CEO:"Mark Zuckerberg", fundedOn: 2004},
+    { name: "Spotify", marketValue: 30, CEO:"Daniel Ek", fundedOn: 2006},
+    { name: "Apple", marketValue: 845, CEO:"Tim Cook", fundedOn: 1976}
+]
